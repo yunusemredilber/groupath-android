de87dc7397ce0def75c9f9ff20ae77469018106e
Yunus Emre Dilber <yunusemredilber@yandex.com>
2019-08-15 01:17:51 +0300

    Add comment screen's fab a function

--- app/src/main/java/com/example/groupath/MessageActivity.java
+++ app/src/main/java/com/example/groupath/MessageActivity.java
@@ -5,11 +5,9 @@ import android.os.Bundle;
 import com.basecamp.turbolinks.TurbolinksSession;
 import com.basecamp.turbolinks.TurbolinksView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
+import android.os.SystemClock;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class MessageActivity extends BaseActivity {
@@ -46,10 +44,17 @@ public class MessageActivity extends BaseActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                focusInput();
             }
         });
+    }
+
+    public void focusInput(){
+        String script = "document.activeElement.blur();" +
+                        "document.getElementById('comment_text').focus();";
+        findViewById(R.id.turbolinks_view).requestFocus();
+        TurbolinksSession.getDefault(this).getWebView()
+                .evaluateJavascript(script,null);
     }
 
 }
